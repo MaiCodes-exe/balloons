b9b09b0af510bf0fec36dbf3a24bf77b3d464134
MaiCodes-exe <maise.b.costa@gmail.com>
2022-09-19 20:26:40 +0000

    ballons is missing color bug

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+const container = document.querySelector(".container");
+
+const singleBalloon = '<div class="balloon"></div>';
+let content = "";
+let balloons = [
+  "white",
+  "red",
+  "green",
+  "yellow",
+  "black",
+  "blue",
+  "purple",
+  "grey",
+  "pink",
+  "teal"
+];
+
+balloons.forEach(function(color, i) {
+  console.log({ color });
+  content =
+    content + '<div class="balloon" style="background: ${color}"></div>';
+});
+container.innerHTML = content;
